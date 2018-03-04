465a3fc85a4d7078aee3ff76daab21067642bd4f
Yako Kobayashi <developer@cleanbuild.jp>
2018-03-04 16:31:47 +0900

    :

--- playSounds/ViewController.swift
+++ playSounds/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer: AVAudioPlayer!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //エクセレント
+    @IBAction func tapExcellent(_ sender: Any) {
+        self.audioPlayer = General.playSound(delegate: self, name: "excellent", type: "mp3")
+    }
+    //グッド
+    @IBAction func tapGood(_ sender: Any) {
+        self.audioPlayer = General.playSound(delegate: self, name: "good", type: "mp3")
+    }
+    //バッド
+    @IBAction func tapBad(_ sender: Any) {
+        self.audioPlayer = General.playSound(delegate: self, name: "bad", type: "mp3")
+    }
+    //正解
+    @IBAction func tapCollect(_ sender: Any) {
+        self.audioPlayer = General.playSound(delegate: self, name: "correct", type: "wav")
+    }
+    //不正解
+    @IBAction func tapIncollect(_ sender: Any) {
+        self.audioPlayer = General.playSound(delegate: self, name: "incorrect", type: "mp3")
+    }
+    
 }
 
